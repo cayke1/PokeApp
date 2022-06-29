64778e101d8d9343207e5e1360421c37e3f8ad28
Cayke Daniel <caykedanielcd@gmail.com>
2022-06-29 07:05:43 -0300

    UseEffect no repeat issue fixed, 'reload' working, modal style complete

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEventHandler } from 'react';
 import Modal from 'react-modal';
 import './Game.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSync } from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 const customStyles = {
@@ -13,25 +15,64 @@ const customStyles = {
       bottom: 'auto',
       marginRight: '-50%',
       transform: 'translate(-50%, -50%)',
+      backgroundColor: '#9200c2cb',
+      border: '3px solid #75fee4'
     },
+    overlay: {
+        backgroundColor: 'rgba(0, 0, 0, 0.4)'
+    }
   };
 
 Modal.setAppElement('#root');
 
+const initialValue = {
+    inputResponse : '',
+}
+
 export function Game() {
     const [modalIsOpen, setModalOpen] = useState(false);
+    const [resValue, setResValue] = useState(initialValue);
     const [pokemonName, setPokemonName] = useState([]);
     const [pokemonImage, setPokemonImage] = useState([]);
+    const [reload, setReload] = useState(0);
+    const [count, setCount] = useState(0);
     const [error, setError] = useState([]);
 
-        useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 150) + 1))
+
+
+    useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 150) + 1))
             .then((res) => res.json())
             .then((data) => {
                 setPokemonName(data.forms[0].name);
                 setPokemonImage(data.sprites.other['official-artwork'].front_default);
+                console.log(pokemonName.toString())
             }).catch(err => setError(err));
-    })
+}, [count, reload]);
+
+
+    
+
+    function onChange (ev: any) {
+        const { value } = ev.target;
+        setResValue(value);
+    }
+
+    function checkResponse () {
+        if (resValue.toString() === pokemonName.toString()) {
+            alert('ok');
+            setCount(count + 100);
+        }
+        else {
+            alert('wrote');
+            setCount(count * 0);
+        }
+    }
+
+    function tryReload() {
+        setReload(Math.random());
+        setCount(count * 0);
+    }
 
     function openModal() {
         setModalOpen(true);
@@ -41,14 +82,13 @@ export function Game() {
         setModalOpen(false);
     }
     
-
     return (
         <div className='gameContent'>
             <p id='bestStreak'></p>
             <h2>Who is that pokemon?</h2>
-            <h4 id='counter'>0</h4>
-            <input type='text' id='response' />
-            <button className='buttons' id='btn-1'>Confirm</button>
+            <h4 id='counter'>{count}</h4>
+            <input type='text' onChange={onChange} id='response' />
+            <button onClick={checkResponse} className='buttons' id='btn-1'>Confirm</button>
             <button onClick={openModal} className='buttons' id='btn-2'>Open Image</button>
             <Modal
             isOpen={modalIsOpen}
@@ -56,12 +96,13 @@ export function Game() {
             style={customStyles}
             contentLabel="Just testing"
             >
+                <span className='close' onClick={closeModal}>&times;</span>
                 <div className='modal-content'>
-                    <span className='close' onClick={closeModal}>&times;</span>
+                    
                     <img src={pokemonImage.toString()} alt="" />
                 </div>
             </Modal>
-            <FontAwesomeIcon className='reloadIcon' icon={faSync} />
+            <FontAwesomeIcon onClick={tryReload} className='reloadIcon' icon={faSync} />
         </div>
     )
 }
